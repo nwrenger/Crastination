2204a7505e35a12a6668a9a8b27550c3168d4b1a
NWrenger <nils@wrenger.net>
2022-11-25 17:58:28 +0100

    :lipstick: Fiexed formatting of the code

--- World/Level/End.gd
+++ World/Level/End.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 func _ready():
-    $AnimationFade.play("FadeIn")
+	$AnimationFade.play("FadeIn")
 
 func _on_Animation_animation_finished(_anim_name):
-    $AnimationFade.play("FadeOut")
-    $CanvasLayer/Text.bbcode_text = "The END?"
+	$AnimationFade.play("FadeOut")
+	$CanvasLayer/Text.bbcode_text = "The END?"
 
-    yield($AnimationFade, "animation_finished")
-    # warning-ignore:return_value_discarded
-    get_tree().change_scene("res://World/Title.tscn")
+	yield($AnimationFade, "animation_finished")
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://World/Title.tscn")
