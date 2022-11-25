50b4cd605939d1c2d81c9518ba60dc8e9fd77898
NWrenger <nils@wrenger.net>
2022-11-25 18:09:13 +0100

    :sparkles: Added a light effect on Teleport

--- Portal/Portal.gd
+++ Portal/Portal.gd
@@ -7,7 +7,8 @@ func _process(_delta):
 	if $Area.get_overlapping_bodies():
 		$Light.show()
 		$AnimationFade.play("FadeOut")
-
+		$Light.energy = 1.8
+		$Light.scale = Vector2(1.5,1.5)
 		yield($AnimationFade, "animation_finished")
 		# warning-ignore:return_value_discarded
 		get_tree().change_scene_to(NextScene)
