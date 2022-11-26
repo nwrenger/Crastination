c1c9b375e516b7a709a62576ff7a5a6de4e67600
NWrenger <nils@wrenger.net>
2022-11-26 18:42:23 +0100

    :lipstick: Muted Music on default

--- UI/MuteButton/Mute.gd
+++ UI/MuteButton/Mute.gd
@@ -1,8 +1,9 @@
 extends Button
 
 func _ready():
-    if AudioServer.is_bus_mute(0):
-        pressed = true
+	AudioServer.set_bus_mute(0, true)
+	if AudioServer.is_bus_mute(0):
+		pressed = true
 
 func toggle(button_pressed):
-    AudioServer.set_bus_mute(0, button_pressed)
+	AudioServer.set_bus_mute(0, button_pressed)
