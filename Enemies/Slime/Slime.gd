c9ddd1d335cd11b926a9611e2b7a522dafc10dff
NWrenger <nils@wrenger.net>
2023-02-26 02:15:54 +0100

    :lipstick: Fixed Hitbox of Slime and Code

--- Enemies/Slime/Slime.gd
+++ Enemies/Slime/Slime.gd
@@ -4,17 +4,6 @@ signal player_killed_slime()
 
 var already_dead := false
 
-func _process(_delta):
-#	print(already_dead)
-	if already_dead == false:
-		if $Slime/dead.get_overlapping_bodies():
-#        	print($Slime/dead.get_overlapping_bodies())
-			get_tree().reload_current_scene()
-
-	if $Slime.position.x > 30:
-		$Slime/Sprite.flip_h = false
-	elif $Slime.position.x < -30:
-		$Slime/Sprite.flip_h = true
 
 func head_entered(_body):
 	if already_dead == false:
@@ -25,3 +14,8 @@ func head_entered(_body):
 		$Slime/Sprite.play("death")
 		yield($Slime/Sprite, "animation_finished")
 		queue_free()
+
+
+func dead_entered(_body):
+	if already_dead == false:
+		get_tree().reload_current_scene()
