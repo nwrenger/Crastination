b7a6a0df6cf6eb91efc685da2325d019d07a7c26
NWrenger <nils@wrenger.net>
2023-02-26 15:03:24 +0100

    :lipstick: Changed Code to AnimationPlayer

--- World/Level/End.gd
+++ World/Level/End.gd
@@ -1,12 +1,7 @@
 extends Node2D
 
 func _ready():
-	$AnimationFade.play("FadeIn")
+	$AnimationFade.play("main")
 
 func _on_Animation_animation_finished(_anim_name):
-	$AnimationFade.play("FadeOut")
-	$CanvasLayer/Text.bbcode_text = "The END?"
-
-	yield($AnimationFade, "animation_finished")
-	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://World/Title.tscn")
