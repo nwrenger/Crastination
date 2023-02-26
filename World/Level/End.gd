10f8d3ada11830f6b2958c6efc46de303c8964b7
NWrenger <nils@wrenger.net>
2023-02-26 19:02:47 +0100

    :liptsick: clear save data on playthrough

--- World/Level/End.gd
+++ World/Level/End.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 func _ready():
 	$AnimationFade.play("main")
+	get_node("/root/Save").level = 0
 
 func _on_Animation_animation_finished(_anim_name):
 	get_tree().change_scene("res://World/Title.tscn")
