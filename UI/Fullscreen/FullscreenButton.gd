85e40ce6e7bc977edbb61017d8bb1a7dfec82b84
NWrenger <nils@wrenger.net>
2023-03-13 18:15:59 +0100

    :lipstick: Added error ignore

--- UI/Fullscreen/FullscreenButton.gd
+++ UI/Fullscreen/FullscreenButton.gd
@@ -4,4 +4,5 @@ func _ready():
 	button_pressed = ((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))
 
 func togglescreen(button_pressed):
+	#ignore error
 	get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (button_pressed) else Window.MODE_WINDOWED
--- UI/Zoom/Zoom.gd
+++ UI/Zoom/Zoom.gd
@@ -4,4 +4,5 @@ func _ready():
 	value = get_node("/root/Save").zoom
 
 func _on_value_changed(value):
+	#ignore error
 	get_node("/root/Save").zoom = value
