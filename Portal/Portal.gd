ca97273d8205fbcea811ae292043b70dd7e58d3b
NWrenger <nils@wrenger.net>
2023-03-16 21:24:34 +0100

    :bug: fixed format and level wrongly saved

--- Portal/Portal.gd
+++ Portal/Portal.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var NextScene: PackedScene
-var TotalLevel := 12
+var TotalLevel := 21
 
 
 func Area_entered(_body):
@@ -9,10 +9,9 @@ func Area_entered(_body):
 	$Light.energy = 3
 	$Light.scale = Vector2(1.5,1.5)
 	await $AnimationFade.animation_finished
-	# warning-ignore:return_value_discarded
 	get_tree().change_scene_to_packed(NextScene)
 
-func _enter_tree():
+func _ready():
 	#todo proof for less work
 	for i in TotalLevel:
 		if get_parent().name == "Level" + str(i):
