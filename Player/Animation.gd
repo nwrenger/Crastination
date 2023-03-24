a4e3c5620b5345452ca8380964b7f56273cb4ac2
NWrenger <nils@wrenger.net>
2023-03-25 00:48:51 +0100

    :bug: Fixed dash not animated

--- Player/Animation.gd
+++ Player/Animation.gd
@@ -32,7 +32,7 @@ func _on_Player_move(velocity, wall_right, wall_left, on_ground, dash_right, das
 		flip_h = false
 	if Input.is_action_just_pressed("ui_up"):
 		nplay("jump")
-	if velocity.y > 0 and wall_left == false and wall_right == false and jump_timer > 0.15:
+	if velocity.y > 0 and wall_left == false and wall_right == false and jump_timer > 0.15 and dash_left == false and dash_right == false:
 			nplay("readyfalling")
 	if Input.is_action_pressed("ui_left") == false and Input.is_action_pressed("ui_right") == false and velocity.y > -1 and velocity.y < 26 and on_ground:
 		if animation == "readyfalling":
